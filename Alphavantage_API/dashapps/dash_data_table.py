c5a6d85c8cacac6d82e7dc03d6388857924dd96c
mheerspink75 <51008045+mheerspink75@users.noreply.github.com>
2019-11-13 21:48:54 -0600

    Update dash_data_table.py

--- Alphavantage_API/dashapps/dash_data_table.py
+++ Alphavantage_API/dashapps/dash_data_table.py
@@ -4,8 +4,8 @@ import pandas_datareader.data as web
 from API_KEYS import ALPHAVANTAGE_API_KEY
 
 API_KEY = 'ALPHAVANTAGE_API_KEY'
-stock = ["AAPL", "MSFT", "GOOG", "AMZN", "FB"]
 
+stock = ["AAPL", "MSFT", "GOOG", "AMZN", "FB"]
 df = web.get_quote_av(stock, api_key=('ALPHAVANTAGE_API_KEY')).reset_index()
 
 app = dash.Dash(__name__)
